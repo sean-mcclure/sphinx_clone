b318758e1ecbef74abd1abb8b97cc7e566b0e491
Sean McClure <seanaaron100@gmail.com>
2021-01-17 23:43:55 +0000

    adding css grid on main page

---
+++ src/LogoSearch.js
@@ -0,0 +1,15 @@
+import "./LogoSearch.css";
+
+function LogoSearch() {
+    return(
+        <div className="logo_search_wrapper">
+            <div className="logo_search_item"></div>
+            <div className="logo_search_item"></div>
+            <div className="logo_search_item"></div>
+            <div className="logo_search_item"></div>
+            <div className="logo_search_item"></div>
+        </div>
+    )
+}
+
+export default LogoSearch;
