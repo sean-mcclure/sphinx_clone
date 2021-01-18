a9e394e280a99c0db99c23aa67d1f0d92f4413d9
Sean McClure <seanaaron100@gmail.com>
2021-01-18 17:36:35 +0000

    working on LogoSearch component

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Sphinx Clone
 
-Sphinx Clone is for creating Sphinx-type documentation without the extensive overhead of the original Sphinx library. Shpinx clone can be used with any language as all function details are listed in a simple JSON file.
+  
+
+Sphinx Clone is for creating Sphinx-type documentation without the extensive overhead of the original Sphinx library (and with some aesthetic improvements). Sphinx clone mimics the [read_the_docs theme](https://sphinx-rtd-theme.readthedocs.io/en/stable/). 
+
+Sphinx Clone is responsive, conforming to different screen sizes and devices.
+
+Sphinx clone can be used with any language as all function details are listed in a simple JSON file.
 
 ## Adding language details
-
-## Downloading the Build.
 
 ## Hosting your Documentation
--- src/LogoSearch.js
+++ src/LogoSearch.js
@@ -1,13 +1,22 @@
 import "./LogoSearch.css";
 
+import {
+    FaHome
+} from "react-icons/fa";
+
 function LogoSearch() {
     return(
         <div className="logo_search_wrapper">
+            <div className="logo_search_item">
+                <div><FaHome className="home_icon"/></div><div className="lib_title">AZLE</div>
+            </div>
             <div className="logo_search_item"></div>
-            <div className="logo_search_item"></div>
-            <div className="logo_search_item"></div>
-            <div className="logo_search_item"></div>
-            <div className="logo_search_item"></div>
+            <div className="logo_search_item">
+                stable
+            </div>
+            <div className="logo_search_item">
+                <input className="search" placeholder="search docs..."></input>
+            </div>
         </div>
     )
 }
